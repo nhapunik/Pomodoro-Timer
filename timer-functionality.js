cd8983f5d012ec882630d08e10eef5f62b687ddc
Niki Hapunik <nikitahapunik@gmail.com>
2023-04-22 22:41:31 +0100

    Add JavaScript functionality for Pomodoro Timer
    - Implement start, stop, reset cycle, reset step, next step, and previous step actions
    - Create an array of step names and durations to manage timer states
    - Update timer display and current step display based on timer progress
    - Handle timer transitions between different steps

---
+++ timer-functionality.js
@@ -0,0 +1,92 @@
+const timerDisplay = document.getElementById('timer');
+const currentStepDisplay = document.getElementById('current-step');
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const resetCycleButton = document.getElementById('reset-cycle');
+const resetStepButton = document.getElementById('reset-step');
+const nextStepButton = document.getElementById('next-step');
+const prevStepButton = document.getElementById('prev-step');
+
+const stepNames = [
+  'Time to Work',
+  'Short Break',
+  'Time to Work',
+  'Short Break',
+  'Time to Work',
+  'Short Break',
+  'Time to Work',
+  'Short Break',
+  'Long Break',
+];
+
+const stepDurations = [25 * 60, 5 * 60, 25 * 60, 5 * 60, 25 * 60, 5 * 60, 25 * 60, 5 * 60, 30 * 60];
+
+let currentStep = 0;
+let timeLeft = stepDurations[currentStep];
+let timerInterval = null;
+
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  seconds %= 60;
+  return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+function updateDisplay() {
+  timerDisplay.textContent = formatTime(timeLeft);
+  currentStepDisplay.textContent = stepNames[currentStep];
+}
+
+function startTimer() {
+  if (!timerInterval) {
+    timerInterval = setInterval(() => {
+      timeLeft--;
+      updateDisplay();
+
+      if (timeLeft <= 0) {
+        currentStep = (currentStep + 1) % stepNames.length;
+        timeLeft = stepDurations[currentStep];
+      }
+    }, 1000);
+  }
+}
+
+function stopTimer() {
+  clearInterval(timerInterval);
+  timerInterval = null;
+}
+
+function resetCycle() {
+  currentStep = 0;
+  timeLeft = stepDurations[currentStep];
+  stopTimer();
+  updateDisplay();
+}
+
+function resetStep() {
+  timeLeft = stepDurations[currentStep];
+  stopTimer();
+  updateDisplay();
+}
+
+function nextStep() {
+  currentStep = (currentStep + 1) % stepNames.length;
+  timeLeft = stepDurations[currentStep];
+  stopTimer();
+  updateDisplay();
+}
+
+function prevStep() {
+  currentStep = (currentStep - 1 + stepNames.length) % stepNames.length;
+  timeLeft = stepDurations[currentStep];
+  stopTimer();
+  updateDisplay();
+}
+
+startButton.addEventListener('click', startTimer);
+stopButton.addEventListener('click', stopTimer);
+resetCycleButton.addEventListener('click', resetCycle);
+resetStepButton.addEventListener('click', resetStep);
+nextStepButton.addEventListener('click', nextStep);
+prevStepButton.addEventListener('click', prevStep);
+
+updateDisplay();
