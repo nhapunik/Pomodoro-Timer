a79a0154fbd3b9d2efeb4ab7da5031e6a40b3bf1
Niki Hapunik <nikitahapunik@gmail.com>
2023-04-23 09:29:09 +0100

    Revert "Combine start and stop buttons with alternating functionality"
    This reverts commit 2012c008a6bbee4ee05de274fa77bd59c9ee629a.

--- timer-functionality.js
+++ timer-functionality.js
@@ -1,6 +1,7 @@
 const timerDisplay = document.getElementById("timer");
 const currentStepDisplay = document.getElementById("current-step");
-const startStopBtn = document.getElementById('start-stop');
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
 const resetCycleButton = document.getElementById("reset-cycle");
 const resetStepButton = document.getElementById("reset-step");
 const nextStepButton = document.getElementById("next-step");
@@ -46,18 +47,6 @@ function updateDisplay() {
   timerDisplay.textContent = formatTime(timeLeft);
   currentStepDisplay.textContent = stepNames[currentStep];
 }
-let timerRunning = false;
-
-startStopBtn.addEventListener('click', () => {
-  timerRunning = !timerRunning;
-  if (timerRunning) {
-    startStopBtn.textContent = 'Stop';
-    startTimer();
-  } else {
-    startStopBtn.textContent = 'Start';
-    stopTimer();
-  }
-});
 
 function startTimer() {
   if (!timerInterval) {
